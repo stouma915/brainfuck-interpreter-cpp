923ba8d04274dbfd21fa7896da8ed39e68aac867
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-14 08:13:10 +0900

    Update interpreter.h

--- src/interpreter.h
+++ src/interpreter.h
@@ -1,6 +1,11 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
 #include <iostream>
 #include <tuple>
 
 #include "memory.h"
 
 std::tuple<std::string, Memory> evaluate(std::string code, Memory memory);
+
+#endif
