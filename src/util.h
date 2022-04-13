4b9d8d915a25d8de3c32ce6d189c9f7ec7d8633e
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-14 08:09:48 +0900

    Update util.h

--- src/util.h
+++ src/util.h
@@ -1,4 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <iostream>
 
 bool startsWith(std::string str, std::string prefix);
 int  search_loop_end(std::string before, std::string after);
+
+#endif UTIL_H
