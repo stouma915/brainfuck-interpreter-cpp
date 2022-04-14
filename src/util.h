418d1649aa2e6ba3032554f086deec7193f46d1f
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-15 08:18:11 +0900

    Update util.h

--- src/util.h
+++ src/util.h
@@ -6,5 +6,6 @@
 bool starts_with(std::string str, std::string prefix);
 bool is_single_bit_int(std::string str);
 int  search_loop_end(std::string code, int start_index);
+int  to_single_bit_int(std::string str);
 
 #endif
