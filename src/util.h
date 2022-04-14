7e60f8c241a54208c72d59a6f167c1ab229978dc
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-15 07:59:42 +0900

    Update util.h

--- src/util.h
+++ src/util.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-bool startsWith(std::string str, std::string prefix);
+bool starts_with(std::string str, std::string prefix);
 int  search_loop_end(std::string code, int start_index);
 
 #endif
