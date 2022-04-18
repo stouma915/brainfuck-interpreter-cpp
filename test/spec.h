8774b2096fda2a1835dd9432874e15dd3577117c
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-18 13:03:38 +0900

    Update spec.h

--- test/spec.h
+++ test/spec.h
@@ -5,7 +5,13 @@
 template <typename A>
 class Spec {
     public:
-        std::string name
+        std::string name;
+        A expect;
+
+        Spec(std::string _name, A _expect) {
+            name = _name;
+            expect = _expect;
+        }
 };
 
-Spec create_spec(std::string name);
+Spec create_spec(std::string name, A expect);
