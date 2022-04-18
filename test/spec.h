1406dfaa59b49bd915bfef09fa6439101e5b3326
stouma915 <sasakitouma@yahoo.ne.jp>
2022-04-18 17:19:41 +0900

    specを実装

--- test/spec.h
+++ test/spec.h
@@ -6,15 +6,37 @@ template <typename A>
 class Spec {
     public:
         std::string name;
+        std::string should;
         A expect;
 
-        Spec(std::string _name, A _expect) {
+        Spec(std::string _name, std::string _should, A _expect) {
             name = _name;
+            should = _should;
             expect = _expect;
         }
 
-        bool test(A x);
-};
+        bool test(A x) {
+            using namespace std;
 
-template <typename A>
-Spec<A> create_spec(std::string name, A expect);
+            cout << "--- " << name << " should " << should << " ---" << endl;
+
+            if (x == expect) {
+                cout << "OK." << endl;
+
+                return true;
+            } else {
+                cout << "Expected '" << expect << "', but the result was '" << x << "'." << endl;
+                cout << "Fail." << endl;
+
+                return false;
+            }
+        }
+
+        static Spec<A> create_spec(std::string _name, std::string _should, A _expect) {
+            return Spec<A>(
+                _name,
+                _should,
+                _expect
+            );
+        }
+};
