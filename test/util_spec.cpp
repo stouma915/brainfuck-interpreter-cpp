fc71aa13004c18b8546ae43f6caed9b091483146
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-20 08:12:58 +0900

    Create util_spec.cpp

---
+++ test/util_spec.cpp
@@ -0,0 +1,18 @@
+#include <util.cpp>
+
+#include "util_spec.h"
+
+Test util_spec() {
+    return Test::create_test(
+        "util.cpp",
+        {
+            []() -> bool {
+                return Spec<bool>::create_spec(
+                           "bool starts_with(string, string)",
+                           "return true if str starts with prefix",
+                           true
+                       ).test(starts_with("AABBCC", "AA"));
+            }
+        }
+    );
+}
