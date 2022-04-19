aa94921c0e536857ce37e8c4a654ff2c8a6d669a
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-20 08:17:31 +0900

    Update util_spec.cpp

--- test/util_spec.cpp
+++ test/util_spec.cpp
@@ -10,8 +10,15 @@ Test util_spec() {
                 return Spec<bool>::create_spec(
                            "bool starts_with(string, string)",
                            "return true if str starts with prefix",
-                           false
+                           true
                        ).test(starts_with("AABBCC", "AA"));
+            },
+            []() -> bool {
+                return Spec<bool>::create_spec(
+                           "bool starts_with(string, string)",
+                           "return false if str doesn't start with prefix",
+                           false
+                       ).test(starts_with("AABBCC", "BB"));
             }
         }
     );
