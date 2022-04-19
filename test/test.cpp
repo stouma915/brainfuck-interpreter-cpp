8e60135a003e913093077630ea374c3ec9412df4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-19 13:04:25 +0900

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
 std::tuple<int, int> Test::run() {
     std::cout << "Start testing '" << name << "'." << std::endl;
 
-    for (int i = 0; i < spec_vec.length(); i ++) {
+    for (int i = 0; i < spec_vec.size(); i ++) {
         if (spec_vec[i]()) {
             success ++;
         } else {
