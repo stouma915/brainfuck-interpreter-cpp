b1ae9fdfe1dbdd10a8f95e7c1c2a13ed6d9028e2
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-20 07:53:41 +0900

    Update ascii_converter_spec.cpp

--- test/ascii_converter_spec.cpp
+++ test/ascii_converter_spec.cpp
@@ -9,9 +9,44 @@ Test ascii_converter_spec() {
             []() -> bool {
                 return Spec<char>::create_spec(
                            "convert_ascii",
-                           "convert int to char",
+                           "convert 65 to A",
                            'A'
                        ).test(convert_ascii(65));
+            },
+            []() -> bool {
+                return Spec<char>::create_spec(
+                           "convert_ascii",
+                           "convert 66 to B",
+                           'B'
+                       ).test(convert_ascii(66));
+            },
+            []() -> bool {
+                return Spec<char>::create_spec(
+                           "convert_ascii",
+                           "convert 67 to C",
+                           'C'
+                       ).test(convert_ascii(67));
+            },
+            []() -> bool {
+                return Spec<char>::create_spec(
+                           "convert_ascii",
+                           "convert 97 to a",
+                           'a'
+                       ).test(convert_ascii(97));
+            },
+            []() -> bool {
+                return Spec<char>::create_spec(
+                           "convert_ascii",
+                           "convert 98 to b",
+                           'b'
+                       ).test(convert_ascii(98));
+            },
+            []() -> bool {
+                return Spec<char>::create_spec(
+                           "convert_ascii",
+                           "convert 99 to c",
+                           'c'
+                       ).test(convert_ascii(99));
             }
         }
     );
