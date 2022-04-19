b23dd5af8ff61513b4bcf0e7afff4778ffec69bd
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-20 07:57:39 +0900

    Update ascii_converter_spec.cpp

--- test/ascii_converter_spec.cpp
+++ test/ascii_converter_spec.cpp
@@ -8,42 +8,42 @@ Test ascii_converter_spec() {
         {
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 65 to A",
                            'A'
                        ).test(convert_ascii(65));
             },
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 66 to B",
                            'B'
                        ).test(convert_ascii(66));
             },
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 67 to C",
                            'C'
                        ).test(convert_ascii(67));
             },
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 97 to a",
                            'a'
                        ).test(convert_ascii(97));
             },
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 98 to b",
                            'b'
                        ).test(convert_ascii(98));
             },
             []() -> bool {
                 return Spec<char>::create_spec(
-                           "convert_ascii",
+                           "char convert_ascii(int)",
                            "convert 99 to c",
                            'c'
                        ).test(convert_ascii(99));
