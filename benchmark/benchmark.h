35b29956b4fbb1db8a4c3a0b2aa633e60181bbba
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-22 07:49:31 +0900

    Update benchmark.h

--- benchmark/benchmark.h
+++ benchmark/benchmark.h
@@ -4,6 +4,5 @@
 #include <vector>
 
 std::vector<long> benchmark(std::string code, int num_of_exec);
-
 void show_running_message(std::string weight, int num_of_exec);
 void show_max_min_average(std::vector<long> vec);
