e5e83c25b3b5a863d86179fd1ab6b14c384924a4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-23 08:16:03 +0900

    Update interpreter_spec.cpp

--- test/interpreter_spec.cpp
+++ test/interpreter_spec.cpp
@@ -14,21 +14,21 @@ Test interpreter_spec() {
                     "tuple<string, Memory> evaluate(string, Memory)",
                     "evaluate Brainf**k code",
                     "A"
-                ).test(std::get<0>(evaluate("----[---->+<]>++.", Memory())));
+                ).test(std::get<0>(evaluate("----[---->+<]>++.", Memory(), 0)));
             },
             []() -> bool {
                 return Spec<std::string>::create_spec(
                     "tuple<string, Memory> evaluate(string, Memory)",
                     "evaluate Brainf**k code",
                     "ABCD"
-                ).test(std::get<0>(evaluate("----[---->+<]>++.+.+.+.", Memory())));
-            },
+                ).test(std::get<0>(evaluate("----[---->+<]>++.+.+.+.", Memory(), 0)));
+            }, thanks 
             []() -> bool {
                 return Spec<std::string>::create_spec(
                     "tuple<string, Memory> evaluate(string, Memory)",
                     "evaluate Brainf**k code",
                     "!@#$"
-                ).test(std::get<0>(evaluate("++++[->++++++++<]>+.-[->++<]>.[-->+<]>+++.+.", Memory())));
+                ).test(std::get<0>(evaluate("++++[->++++++++<]>+.-[->++<]>.[-->+<]>+++.+.", Memory(), 0)));
             }
         }
     );
