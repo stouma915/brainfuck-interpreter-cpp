268cae8f03731139c1706cdcc1e5a616962cfe11
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-23 08:11:45 +0900

    Update interpreter.h

--- src/interpreter.h
+++ src/interpreter.h
@@ -5,4 +5,7 @@
 
 #include "memory.h"
 
-std::tuple<std::string, Memory> evaluate(std::string code, Memory memory);
+std::tuple<std::string, Memory> evaluate(std::string code,
+                                         Memory memory,
+                                         int start_index = 0
+);
