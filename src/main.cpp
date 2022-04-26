6c5d894b3e30acead331424f6b8f2a7d67ea102e
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-04-27 08:17:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "interpreter.h"
 #include "util.h"
 
-#define VERSION "1.0.0"
+#define VERSION "1.0.3"
 
 void show_usage(std::string file_name) {
     std::cout << "Brainf**k Interpreter " << VERSION << "\n"
