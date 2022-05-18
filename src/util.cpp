54abc2ef74cf7f5f9f6dd55f7b60edb4ba42d47b
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-19 08:03:54 +0900

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -6,11 +6,11 @@ bool starts_with(std::string str, std::string prefix) {
     return str.rfind(prefix, 0) == 0;
 }
 
-bool is_single_bit_int(std::string str) {
-    if (regex_match(str, std::regex("[+-]?[0-9]{1,3}"))) {
+bool is_int(std::string str) {
+    if (regex_match(str, std::regex("[+-]?[0-9]{1,10}"))) {
         long num = atol(str.c_str());
 
-        if (num >= -128 && num <= 127) {
+        if (num >= -2147483648L && num <= 2147483647L) {
             return true;
         } else {
             return false;
@@ -48,7 +48,7 @@ int search_loop_end(std::string code, int start_index) {
     return result;
 }
 
-int to_single_bit_int(std::string str) {
+int to_int(std::string str) {
     long num = atol(str.c_str());
 
     return (int) num;
