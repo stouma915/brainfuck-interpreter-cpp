53b8c69a63d84696992cc1c6a47f3233e3a06345
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-24 07:53:29 +0900

    Update util.h

--- benchmark/util.h
+++ benchmark/util.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 long current_epoch_milli();
+long sum(std::vector<long> vec);
 long max(std::vector<long> vec);
 long min(std::vector<long> vec);
 long average(std::vector<long> vec);
-long sum(std::vector<long> vec);
