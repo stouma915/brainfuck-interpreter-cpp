887a875a161d78c76c84ec44813ee6520c913ef7
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-25 08:15:26 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,10 @@ CPP_VERSION=17
 
 mkdir -p out
 
-g++ -I src test/*.cpp -o out/interpreter_test -Wall -std=c++$CPP_VERSION -O3 && out/interpreter_test
+g++ -I src\
+    test/*.cpp\
+    -o out/interpreter_test\
+    -Wall\
+    -std=c++$CPP_VERSION\
+    -O3\
+    && out/interpreter_test
