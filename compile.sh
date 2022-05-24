607228f0d88d7a957b64d8a20f65fce87cca930b
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-25 08:18:37 +0900

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,4 +4,8 @@ CPP_VERSION=17
 
 mkdir -p out
 
-g++ src/*.cpp -o out/brainfuck-interpreter -Wall -std=c++$CPP_VERSION -O3
+g++ src/*.cpp\
+    -o out/brainfuck-interpreter\
+    -Wall\
+    -std=c++$CPP_VERSION\
+    -O3
