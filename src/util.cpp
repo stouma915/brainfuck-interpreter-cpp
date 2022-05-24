f8e08f8a4f2b5faa555824dc91d24c47b4aaf8cd
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-24 15:14:21 +0900

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -27,7 +27,7 @@ int search_loop_end(std::string code, int start_index) {
 
     int result = -1;
 
-    int x, y = 0;
+    int x, y;
 
     for (unsigned int i = start_index; i < code.length(); i ++) {
         char symbol = code[i];
