210dd89f0d6f1dfb88385d904016291c320121da
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-25 08:17:11 +0900

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,10 @@ CPP_VERSION=17
 
 mkdir -p out
 
-g++ -I src benchmark/*.cpp -o out/interpreter_bench -Wall -std=c++$CPP_VERSION -O3 && out/interpreter_bench
+g++ -I src\
+    benchmark/*.cpp\
+    -o out/interpreter_bench\
+    -Wall\
+    -std=c++$CPP_VERSION\
+    -O3\
+    && out/interpreter_bench
