82070a12a9e9580e08857c147995133b8a8c6fa2
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-25 08:01:41 +0900

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 #include "test.h"
 
-Result Test::run() {
+TestResult Test::run() {
     std::cout << "Start testing '" << name << "'." << std::endl;
 
     for (unsigned int i = 0; i < spec_vec.size(); i ++) {
@@ -11,5 +11,5 @@ Result Test::run() {
         }
     }
 
-    return Result(success, failure);
+    return TestResult(success, failure);
 }
