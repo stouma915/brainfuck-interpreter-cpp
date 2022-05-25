4686836779c6a7a8641e10124d993a7cc389c361
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-26 08:20:00 +0900

    Update interpreter_spec.cpp

--- test/interpreter_spec.cpp
+++ test/interpreter_spec.cpp
@@ -8,25 +8,27 @@
 #include "spec.h"
 
 Test interpreter_spec() {
+    using namespace std;
+
     return Test::create_test(
         "interpreter.cpp",
         {
             []() -> bool {
-                return Spec<std::string>::create_spec(
+                return Spec<string>::create_spec(
                     "Result evaluate(string, Memory, unsigned int)",
                     "evaluate Brainf**k code",
                     "A"
                 ).test(evaluate("----[---->+<]>++.", Memory(), 0).output);
             },
             []() -> bool {
-                return Spec<std::string>::create_spec(
+                return Spec<string>::create_spec(
                     "Result evaluate(string, Memory, unsigned int)",
                     "evaluate Brainf**k code",
                     "ABCD"
                 ).test(evaluate("----[---->+<]>++.+.+.+.", Memory(), 0).output);
             },
             []() -> bool {
-                return Spec<std::string>::create_spec(
+                return Spec<string>::create_spec(
                     "Result evaluate(string, Memory, unsigned int)",
                     "evaluate Brainf**k code",
                     "!@#$"
